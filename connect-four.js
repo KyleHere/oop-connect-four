791674f5a8d90fc38e62c1016455c53d0ba0b303
Kyle Tseng <ktseng122@gmail.com>
2021-04-07 16:59:30 -0400

    complete phase 2

--- connect-four.js
+++ connect-four.js
@@ -4,11 +4,22 @@ let game = undefined;
 function updateUI() {
   const boardHolder = document.getElementById('board-holder');
   const gameName = document.getElementById('game-name');
+  const newTargets = document.getElementById('click-targets');
   if (game === undefined) {
     boardHolder.classList.add('is-invisible');
   } else {
     boardHolder.classList.remove('is-invisible');
     gameName.innerHTML = game.getName();
+  }
+
+
+  if(game.currentPlayer === 1){
+    newTargets.classList.add('black')
+    newTargets.classList.remove('red')
+  }
+  else{
+    newTargets.classList.add('red')
+    newTargets.classList.remove('black')
   }
 }
 
@@ -16,6 +27,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const playerOneInput = document.getElementById('player-1-name');
   const playerTwoInput = document.getElementById('player-2-name');
   const newGameStatus = document.getElementById('new-game');
+  const targets = document.getElementById('click-targets')
 
   playerOneInput.addEventListener('keyup', (event) => {
     if (playerOneInput.value.length > 0 && playerTwoInput.value.length > 0) {
@@ -36,4 +48,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     newGameStatus.disabled = true;
     updateUI();
   });
+
+  targets.addEventListener('click', event =>{
+    game.playInColumn();
+    updateUI();
+  })
 });
--- game.js
+++ game.js
@@ -1,9 +1,20 @@
 export default class Game {
-  constructor(playerOneName, playerTwoName) {
+  constructor(playerOneName, playerTwoName, currentPlayer) {
     this.playerOneName = playerOneName;
     this.playerTwoName = playerTwoName;
+    this.currentPlayer = 1;
   }
+
   getName() {
     return `${this.playerOneName} vs ${this.playerTwoName}`;
+  }
+
+  playInColumn(){
+    if(this.currentPlayer === 1){
+      this.currentPlayer = 2;
+    }
+    else{
+      this.currentPlayer = 1;
+    }
   }
 }
